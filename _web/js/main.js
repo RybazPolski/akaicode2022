b288321ee944fffd754c26d6cf42a79634598f2e
Ryba <julorybarczyk@gmail.com>
2022-11-05 13:57:52 +0100

    no backend final i guess

--- _web/js/main.js
+++ _web/js/main.js
@@ -73,6 +73,8 @@ envf.addSprite("midControls","noticeboardRemove","graphics/paperScrapDR.png",{x:
 $("#noticeboardRemove").html("&#128465;")
 envf.addSprite("midControls","noticeboardClose","graphics/paperScrapD.png",{x:2,y:125,width:16.65,height:16.03,z:20})
 $("#noticeboardClose").html("-")
+$("#noticeboardClose").css('padding-top','0')
+
 
 var noticeEdit = false, oldDesc, oldTitle
 $('#noticeboardDesc').on("click",(()=>{
@@ -137,6 +139,22 @@ $('#noticeboardCancel').on("click",(()=>{
     
 }))
 
+$('#ogloszenieTitle, #ogloszenieDesc').on('focus',()=>{
+    if(noticeEdit){
+        $('#noticeboardL,#noticeboardR').each(function(){
+            $(this).css('display','none')
+        })
+    }
+})
+$('#ogloszenieTitle, #ogloszenieDesc').on('focusout',()=>{
+    if(noticeEdit){
+        $('#noticeboardL,#noticeboardR').each(function(){
+            $(this).css('display','block')
+        })
+    }
+})
+
+
 envf.addSprite("right","taskboard","graphics/taskboard.png",{x:45.5,y:105,width:240,height:315.6,z:10})
 $('#taskboard').html("<table id='taskTable'><tr><td id='task1'></td></tr><tr><td id='task2'></td></tr><tr><td id='task3'></td></tr><table>")
 envf.addSprite("lControls","taskboardU","graphics/paperScrapD.png",{x:180,y:140,width:16.65,height:16.03,z:20})
@@ -147,6 +165,7 @@ envf.addSprite("lControls","taskboardAdd","graphics/paperScrapDG.png",{x:180,y:1
 $("#taskboardAdd").html("+")
 envf.addSprite("lControls","taskboardClose","graphics/paperScrapD.png",{x:102,y:112.5,width:16.65,height:16.03,z:20})
 $("#taskboardClose").html("-")
+$("#taskboardClose").css('padding-top','0')
 envf.addSprite("task1","taskA","graphics/paperScrapB.png",{x:180,y:120,width:50.375,height:35,z:20})
 envf.addSprite("task2","taskB","graphics/paperScrapB.png",{x:180,y:140,width:50.375,height:35,z:20})
 envf.addSprite("task3","taskC","graphics/paperScrapB.png",{x:180,y:160,width:50.375,height:35,z:20})
@@ -194,6 +213,8 @@ $("#task").html(`
 
 envf.addSprite("lControls","taskClose","graphics/paperScrapD.png",{x:102,y:130,width:16.65,height:16.03,z:35})
 $("#taskClose").html("-")
+$("#taskClose").css('padding-top','0')
+
 
 envf.addSprite("lControls","taskEdit","graphics/paperScrapDY.png",{x:182,y:130,width:16.65,height:16.03,z:35})
 $("#taskEdit").html("&#9999;")
@@ -281,3 +302,132 @@ $('#taskClose').on("click",(()=>{
     }
 })
 )
+
+
+
+
+
+
+
+
+
+
+envf.addSprite("left","mytasks","graphics/scrollRozwiniety.png",{x:-90,y:102.5,width:80,height:201.135,z:10})
+$('#mytasks').html("<table id='mytasksTable'><tr><td id='mytask1'></td></tr><tr><td id='mytask2'></td></tr><tr><td id='mytask3'></td></tr><table>")
+envf.addSprite("lControls","mytasksU","graphics/paperScrapC.png",{x:-70,y:110,width:40,height:14.23,z:15})
+$("#mytasksU").html("&uArr;")
+envf.addSprite("lControls","mytasksD","graphics/paperScrapC.png",{x:-70,y:187.5,width:40,height:14.23,z:15})
+$("#mytasksD").html("&dArr;")
+envf.addSprite("lControls","mytasksClose","graphics/paperScrapD.png",{x:-92.5,y:105,width:16.65,height:16.03,z:20})
+$("#mytasksClose").css('padding-top','0')
+$("#mytasksClose").html("-")
+
+$("#scrollPedestal").on("click",()=>{
+    $('#vignette').css('display', 'block')
+    envf.moveSprite("mytasks","up")
+    envf.moveSprite("mytasksU","up")
+    envf.moveSprite("mytasksD","up")
+    envf.moveSprite("mytasksClose","up")
+})
+
+envf.addSprite("right","mytask","graphics/paperScrapB.png",{x:5-100,y:135,width:90,height:62.53,z:30})
+$("#mytask").html(`
+    <table><tr><th>
+        <input readonly id='mytaskTitle' placeholder='Tytuł'>
+    </th><td>Trudność:<input readonly id='mytaskDifficulty' min='1' max='5' value='1'></td></tr><tr><td colspan="2">
+        <textarea readonly id='mytaskDesc' placeholder='Opis'></textarea>
+    </td></tr><tr><td id='mytaskUsers'>
+        <img class='user' src='https://cetcc.com.br/giga_site_git/public/cetcc/img/user/user.png'> 
+    </td><td>
+        <input type='date' readonly id='mytaskDue'>
+    </td></tr></table>
+`)
+
+envf.addSprite("lControls","mytaskClose","graphics/paperScrapD.png",{x:2-100,y:130,width:16.65,height:16.03,z:35})
+$("#mytaskClose").html("-")
+$("#mytaskClose").css('padding-top','0')
+
+
+envf.addSprite("lControls","mytaskEdit","graphics/paperScrapDY.png",{x:82-100,y:130,width:16.65,height:16.03,z:35})
+$("#mytaskEdit").html("&#9999;")
+$("#mytaskEdit").css('padding-top','0.5%')
+$("#mytaskEdit").css('padding-left','3%')
+envf.addSprite("lControls","mytaskConfirm","graphics/paperScrapDG.png",{x:48-100,y:130,width:16.65,height:16.03,z:35})
+$("#mytaskConfirm").html("&#128190;")
+$("#mytaskConfirm").css('padding-top','0.5%')
+$("#mytaskConfirm").css('padding-left','3%')
+envf.addSprite("lControls","mytaskCancel","graphics/paperScrapDY.png",{x:65-100,y:130,width:16.65,height:16.03,z:35})
+$("#mytaskCancel").html("&#10008;")
+envf.addSprite("lControls","mytaskRemove","graphics/paperScrapDR.png",{x:82-100,y:130,width:16.65,height:16.03,z:35})
+$("#mytaskRemove").html("&#128465;")
+
+envf.addSprite("lControls","mytaskDone","graphics/paperScrapDG.png",{x:42-100,y:160,width:16.65,height:16.03,z:35})
+$("#mytaskDone").html("&#10003;")
+
+$('#mytask1, #mytask2, #mytask3').on("click",(()=>{
+    $('#vignette').css('z-index', '25')
+    envf.moveSprite("mytask","up")
+    envf.moveSprite("mytaskClose","up")
+    envf.moveSprite("mytaskEdit","up")
+    envf.moveSprite("mytaskDone","up")
+
+}))
+
+$('#mytaskClose').on("click",(()=>{
+    $('#vignette').css('z-index', '5')
+    envf.moveSprite("mytask","down")
+    envf.moveSprite("mytaskClose","down")
+    if(mytaskEdit){
+        envf.moveSprite("mytaskCancel","down")
+        envf.moveSprite("mytaskConfirm","down")
+        envf.moveSprite("mytaskRemove","down")            
+        mytaskEdit = false
+        document.querySelector("#mytaskDesc").readOnly = true
+        document.querySelector("#mytaskTitle").readOnly = true
+        document.querySelector("#mytaskDue").readOnly = true
+        document.querySelector("#mytaskDifficulty").readOnly = true
+        document.querySelector("#mytaskDesc").value = oldDesc
+        document.querySelector("#mytaskTitle").value = oldTitle 
+        document.querySelector("#mytaskDue").value = oldDue
+        document.querySelector("#mytaskDifficulty").value = oldDifficulty
+    }else{
+        envf.moveSprite("mytaskDone","down")
+        envf.moveSprite("mytaskDone","down")
+    }
+})
+)
+$('#mytaskEdit').on("click",(()=>{
+    mytaskEdit = true
+    oldDesc = document.querySelector("#mytaskDesc").value
+    oldTitle = document.querySelector("#mytaskTitle").value
+    oldDue = document.querySelector("#mytaskDue").value
+    oldDifficulty = document.querySelector("#mytaskDifficulty").value
+    document.querySelector("#mytaskDesc").readOnly = false
+    document.querySelector("#mytaskTitle").readOnly = false
+    document.querySelector("#mytaskDue").readOnly = false
+    document.querySelector("#mytaskDifficulty").readOnly = false
+    envf.moveSprite("mytaskEdit","down")
+    envf.moveSprite("mytaskDone","down")
+
+    envf.moveSprite("mytaskCancel","up")
+    envf.moveSprite("mytaskConfirm","up")
+    envf.moveSprite("mytaskRemove","up")
+}))
+
+$('#mytaskCancel').on("click",(()=>{
+    mytaskEdit = false
+    document.querySelector("#mytaskDesc").readOnly = true
+    document.querySelector("#mytaskTitle").readOnly = true
+    document.querySelector("#mytaskDue").readOnly = true
+    document.querySelector("#mytaskDifficulty").readOnly = true
+    document.querySelector("#mytaskDesc").value = oldDesc
+    document.querySelector("#mytaskTitle").value = oldTitle 
+    document.querySelector("#mytaskDue").value = oldDue
+    document.querySelector("#mytaskDifficulty").value = oldDifficulty
+    envf.moveSprite("mytaskEdit","up")
+    envf.moveSprite("mytaskDone","up")
+
+    envf.moveSprite("mytaskCancel","down")
+    envf.moveSprite("mytaskConfirm","down")
+    envf.moveSprite("mytaskRemove","down")
+}))
